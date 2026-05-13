653e164b5800712393463a2842fb54ecbc7cd8a9
Andrey-521 <Lutcyk.andry@gmail.com>
2026-05-13 13:41:37 +0700

    Update main4.cpp

--- main4.cpp
+++ main4.cpp
@@ -1,14 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <map>
+
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    unordered_map<string,int> cnt;
-    string s;
-    while (cin >> s) {
-        cout << ++cnt[s] << '\n';
+void Count_Words(const vector<string> words) {
+    map<string, int> counter;
+    for (auto& word : words) {
+        int count = ++counter[word];
+        cout << count << endl;
     }
+    cout << endl;
+}
+
+int main() {
+    vector<string> words;
+    string word;
+    while (cin >> word && word != "-") {
+        words.push_back(word);
+    }
+
+    if (words.empty()) {
+        cout << "No words.\n";
+    }
+    else Count_Words(words);
+
     return 0;
 }
