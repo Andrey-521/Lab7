02133f66173ca1f7269d92629735c2f385ed8a0d
Andrey-521 <Lutcyk.andry@gmail.com>
2026-05-13 13:57:31 +0700

    Update main6.cpp

--- main6.cpp
+++ main6.cpp
@@ -1,29 +1,34 @@
 #include <algorithm>
 #include <vector>
 #include <iostream>
+#include <string>
 
+using namespace std;
 template<typename Iter>
+
 void PrintResults(Iter first, Iter last) {
     for (Iter it = first; it != last; ++it) {
-        std::cout << *it << std::endl;
+        cout << *it << endl;
     }
 }
 
 template<typename T>
 void Process(const std::vector<T>& data) {
-    std::vector<T> filtered;
-
-    std::copy_if(
-        data.begin(), data.end(),
-        std::back_inserter(filtered),
-        [](const T& x) { return x > 0; }
-    );
-
+    vector<T> filtered;
+    filtered.reserve(data.size());
+    copy_if(data.begin(), data.end(),
+        back_inserter(filtered),
+        [](const T& x) { return x > 0; });
+    
     PrintResults(filtered.begin(), filtered.end());
 }
 
 int main() {
-    std::vector<int> v = { -2, 0, 3, 5, -1 };
-    Process(v);
+    vector<int> numbers = { -2, 0, 3, 5, -1 };
+    Process(numbers);
+
+    vector<double> doubles = { -3.1, 1.4, 0.0, 2.9, -0.0 };
+    Process(doubles);
+    
     return 0;
 }
