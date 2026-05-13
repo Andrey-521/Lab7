5dc5ceba361e6a498c555a94b3a4a1b1722a9b79
Andrey-521 <Lutcyk.andry@gmail.com>
2026-05-13 13:40:41 +0700

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -1,37 +1,46 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <algorithm>
+
 using namespace std;
 
+void Alphabet(const vector<string> words) {
+    set<char> common;
+    for (char ch : words[0]) {
+        common.insert(ch);
+    }
+
+    for (size_t i = 1; i < words.size(); ++i) {
+        set<char> current;
+        for (char ch : words[i]) {
+            current.insert(ch);
+        }
+        set<char> new_common;
+        set_intersection(common.begin(), common.end(),
+            current.begin(), current.end(),
+            inserter(new_common, new_common.begin()));
+        common.swap(new_common);
+    }
+
+    for (char ch : common) {
+        cout << ch;
+    }
+    cout << endl;
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    string s;
-    if (!(cin >> s)) return 0;
-
-    vector<bool> intersect(26, false);
-    for (char c : s) {
-        if (isalpha((unsigned char)c)) {
-            c = tolower(c);
-            intersect[c - 'a'] = true;
-        }
+    vector<string> words;
+    string word;
+    while (cin >> word && word != "-") {
+        words.push_back(word);
     }
-
-    while (cin >> s) {
-        vector<bool> present(26, false);
-        for (char c : s) {
-            if (isalpha((unsigned char)c)) {
-                c = tolower(c);
-                present[c - 'a'] = true;
-            }
-        }
-        for (int i = 0; i < 26; ++i) {
-            if (!present[i]) intersect[i] = false;
-        }
+   
+    if (words.empty()) {
+        cout << "No words.\n";
     }
+    else Alphabet(words);
 
-    for (int i = 0; i < 26; ++i) {
-        if (intersect[i]) cout << char('a' + i);
-    }
-    cout << '\n';
     return 0;
-}   
+}
